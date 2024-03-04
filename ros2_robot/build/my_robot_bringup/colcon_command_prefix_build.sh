2b2356f75eef21355573f4dacf77c54757cfe0c9
omom77 <sakharkarom777@gmail.com>
2024-03-04 10:52:30 +0530

    updated code with gazebo xml files

---
+++ ros2_robot/build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/om/ros2_robot/install/my_robot_description/share/my_robot_description/package.sh"
---
+++ ros2_robot/install/my_robot_bringup/share/my_robot_bringup/cmake/my_robot_bringupConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/om/ros2_robot/build/my_robot_bringup/ament_cmake_core/my_robot_bringupConfig-version.cmake
